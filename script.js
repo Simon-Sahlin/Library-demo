2d0b5dc5411c66841553d3edefde64246c4d146e
Simon-Sahlin <simonsahlin@somin.se>
2024-03-03 20:20:16 +0100

    Add HTML and CSS for Books in Book List

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+let myLibrary = [];
+
+function book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+book.prototype.info = function(){
+    return (this.title + " by " + this.author + ", " + this.qpages + " pages, " + (this.read ? "has been read" : "not read yet"));
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let newBook = new book(title, author, pages, read)
+    myLibrary.push(newBook);
+}
